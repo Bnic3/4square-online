98b98ca46fe6a39b06316afc4037191109936bc4
Bnic3 <bnicemagnolia@gmail.com>
2016-10-13 17:03:05 +0100

    integration-test with supertest config

--- test/api/userSpec.js
+++ test/api/userSpec.js
@@ -1,11 +1,14 @@
 /**
  * Created by john.nana on 10/10/2016.
  */
-require('dotenv').config();
 const request = require('supertest');
 const expect = require('chai').expect,
     mongoose= require('mongoose'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    rek= require('rekuire'),
+    app = rek("app"),
+   agent = request.agent(app),
+    User = rek("User");
 
 
 Promise.promisifyAll(mongoose);
@@ -17,9 +20,34 @@ const connectDB = Promise.promisify(mongoose.connect, mongoose);
 describe("User login test", ()=>{
 
     before(()=>{
-        connectDB("mongodb://localhost/4square").then(()=>{ console.log("asyncworked")})
+       /* connectDB("mongodb://localhost/4square").then(()=>{ console.log("asyncworked")})*/
  });//end before
 
-   it("it should sign user up")
-   it('')
+    var user = {
+        "firstname": "John",
+        "lastname": "Nana",
+        "email": "bnicemagnolia@gmail.com",
+        "phoneNo": 8039704763,
+        "address": "37,akinola,Bariga, Lagos",
+        "gender": "Male",
+        "role":"member"
+    };
+   it("it should allow user sign up if validated", ()=>{
+       agent.post('/signup')
+       .send(user)
+       .expect(200)
+       .end((err, results)=>{
+               if(err) console.log(err);
+               results.body.to.have.property('token');
+           })
+
+
+
+   })
+   afterEach((done)=>{
+       User.remove().exec();
+       done();
+
+   })
+
 });
